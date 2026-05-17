737ac558a351b66afc2ca55b499259ce98032da7
Ann Kariuki <awk19@duke.edu>
2026-05-17 16:14:11 -0700

    pagination

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).sort('-name price')
+    const products = await Product.find({})
+        .sort('name')
+        .select('name price')
+        .limit(4)
+        .skip(1)
     res.status(200).json({ products, numHits: products.length })
 }
 
@@ -17,7 +21,7 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' }
     }
-    console.log(queryObject) 
+    console.log(queryObject)
     let result = Product.find(queryObject)
     //sort
     if (sort) {
@@ -29,10 +33,19 @@ const getAllProducts = async (req, res) => {
         result = result.sort('createAt')
     }
 
-    if(fields) {
+    //fields to be returned
+    if (fields) {
         const fieldsList = fields.split(',').join(' ')
         result = result.select(fieldsList)
     }
+    //pagination
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit)
+    //23
+    //23 /7 = 4 pages -> 7,7,7,2
     const products = await result
 
     res.status(200).json({ products, numHits: products.length })
